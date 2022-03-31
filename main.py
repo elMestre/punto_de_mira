92a06ec6ed67d5ac09af56b4279789a853abc25e
Carlos <carmesce@gmail.com>
2022-03-31 13:45:39 +0200

    Dibuja multiples puntos

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 
-def punto_de_mira():
-    plt.plot([0], [0], 'k+')
+def punto_de_mira(lista_de_coordenadas):
+    for coordenadas in lista_de_coordenadas:
+        plt.plot(coordenadas[0], coordenadas[1], 'bo')
+
+    plt.plot(0, 0, 'k+')
     plt.ylabel('Y')
     plt.xlabel('X')
     plt.show()
-    print("Hola Punto de mira")
+
 
 if __name__ == '__main__':
-    punto_de_mira()
+    lista_de_coordenadas = [[7, 3], [-2, 5], [-3, 3 ], [8, -1]]
+    punto_de_mira(lista_de_coordenadas)
