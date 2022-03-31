8367ea4e341ed2c5dd65dde2412f6bc9ee0ad203
Carlos <carmesce@gmail.com>
2022-03-31 12:03:59 +0200

    Añadido pyplot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import matplotlib.pyplot as plt
+
 def punto_de_mira():
+    plt.plot([0], [0], 'k+')
+    plt.ylabel('Y')
+    plt.xlabel('X')
+    plt.show()
     print("Hola Punto de mira")
 
 if __name__ == '__main__':
     punto_de_mira()
-
